fad27cf3f6ca9e44413a9164437b838955272f87
digg0 <diego.pedrosa253@gmail.com>
2025-08-23 09:34:16 -0300

    Primeira versao de login

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.R
+import com.example.myapplication.SecondActivity
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.example.myapplication.databinding.Tela1Binding
+import com.google.firebase.auth.FirebaseAuth
+
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: Tela1Binding
+    private val auth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = Tela1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val botaoFunc = findViewById<Button>(R.id.botao_func)
+
+        botaoFunc.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.botaoEntrar.setOnClickListener{
+            val email = binding.campoUsuario.text.toString()
+            val senha = binding.campoSenha.text.toString()
+
+            if(email.isEmpty() || senha.isEmpty()){
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener{ autenticacao ->
+                    if (autenticacao.isSuccessful){
+                        navegarTelaPrincipal()
+
+                    }
+
+                }.addOnFailureListener(){
+                    Toast.makeText(this, "Erro ao fazer login", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
+       binding.botaoCadastro.setOnClickListener{
+           val intent = Intent(this, TelaCadastro::class.java)
+           startActivity(intent)
+       }
+
+
+
+    }
+    private fun navegarTelaPrincipal(){
+        val intent = Intent (this, TelaTeste::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val usuarioAtual= FirebaseAuth.getInstance().currentUser
+
+        if (usuarioAtual != null){
+            navegarTelaPrincipal()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TelaCadastro.kt
@@ -0,0 +1,96 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.graphics.Color
+import com.example.myapplication.databinding.CadastroBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firestore.v1.StructuredQuery
+
+
+
+
+class TelaCadastro : ComponentActivity() {
+    private lateinit var binding: CadastroBinding
+    private val auth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = CadastroBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
+
+
+        binding.botaoCadastrara.setOnClickListener{ view ->
+
+            val email=binding.cadEmail.text.toString()
+            val senha= binding.cadSenha.text.toString()
+            val nome = binding.cadNome.text.toString()
+            val contato = binding.cadContato.text.toString()
+
+            if(email.isEmpty()|| senha.isEmpty() ){
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show()
+
+            }else{
+                auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener{ cadastro ->
+                    if (cadastro.isSuccessful){
+                        Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
+                        binding.cadEmail.setText("")
+                        binding.cadSenha.setText("")
+                        voltarTelaLogin()
+                    }
+                }.addOnFailureListener{exceptions ->
+
+                    val mensagemErro = when(exceptions){
+                        is FirebaseAuthWeakPasswordException -> "Digite uma Senha com no minimo 6 Caracteres!"
+                        is FirebaseAuthInvalidCredentialsException -> "Digite um Email valido!"
+                        is FirebaseAuthUserCollisionException -> "Essa conta ja foi cadastrada!"
+                        is FirebaseNetworkException -> "Sem Conexao com Internet!"
+                        else -> "Erro ao cadastrar usuario!"
+                    }
+                    Toast.makeText(this, mensagemErro, Toast.LENGTH_SHORT).show()
+
+
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+    private fun voltarTelaLogin(){
+        val intent = Intent (this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
